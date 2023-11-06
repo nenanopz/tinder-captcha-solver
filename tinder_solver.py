db826472273557807238e9eb9db27c8f2b8da5b0
nenanopz <147850398+nenanopz@users.noreply.github.com>
2023-11-06 17:34:36 +0100

    Update tinder_solver.py

--- tinder_solver.py
+++ tinder_solver.py
@@ -4,20 +4,19 @@ from urllib.parse import urlparse
 # Change this information
 capsolver.api_key = "Your pay per usage key"
 
-def solve_funcaptcha_linkedin():
+def solve_funcaptcha_tinder():
     solution = capsolver.solve({
     "type": "FunCaptchaTaskProxyLess",
-        "websiteURL": "https://iframe.arkoselabs.com",
-        "websitePublicKey": "3117BF26-4762-4F5A-8ED9-A85E69209A46",
-        "funcaptchaApiJSSubdomain": "https://client-api.arkoselabs.com",
-        "data": "{\"blob\":\"Obtain this value, it's required and it's different each time.\"}"
+        "websiteURL": "https://tinder.com/",
+        "websitePublicKey": "15485B1D-E5A1-0166-162C-FF8EBF968955",
+        "funcaptchaApiJSSubdomain": "https://tinder-api.arkoselabs.com/"
     })
     return solution
 
 def main():
     
-    print("Solving linkedin captcha")
-    solution = solve_funcaptcha_linkedin()
+    print("Solving tinder captcha")
+    solution = solve_funcaptcha_tinder()
     
     token = solution["token"]
     
